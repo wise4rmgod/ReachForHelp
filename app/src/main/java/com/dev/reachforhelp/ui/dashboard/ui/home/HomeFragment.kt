7b9be44c29577b4609a4ffa4845bc03070e04055
Peculiar Umeh <peculiarumeh02@gmail.com>
2022-02-12 19:34:39 +0100

    Fix Family/Friends Fragments

--- app/src/main/java/com/dev/reachforhelp/ui/dashboard/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dev/reachforhelp/ui/dashboard/ui/home/HomeFragment.kt
@@ -8,8 +8,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.dev.reachforhelp.ui.dashboard.R
-import com.dev.reachforhelp.ui.dashboard.databinding.FragmentHomeBinding
+import com.dev.reachforhelp.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -31,7 +31,7 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        val textView: TextView = binding.tex
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
--- app/src/main/java/com/dev/reachforhelp/ui/dashboard/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/dev/reachforhelp/ui/dashboard/ui/notifications/NotificationsFragment.kt
@@ -8,8 +8,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.dev.reachforhelp.ui.dashboard.R
-import com.dev.reachforhelp.ui.dashboard.databinding.FragmentNotificationsBinding
+import com.dev.reachforhelp.databinding.FragmentNotificationsBinding
+
 
 class NotificationsFragment : Fragment() {
 
