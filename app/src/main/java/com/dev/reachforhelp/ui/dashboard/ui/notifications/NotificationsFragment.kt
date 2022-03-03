0b52d233d07c0c9e10fb7d91760bf2a0687c9724
Peculiaruc <peculiarumeh02@gmail.com>
2022-03-03 23:15:14 +0100

    Implemented Help Screen, Dashboard Screen, Notification Screen

--- app/src/main/java/com/dev/reachforhelp/ui/dashboard/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/dev/reachforhelp/ui/dashboard/ui/notifications/NotificationsFragment.kt
@@ -4,20 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.dev.reachforhelp.R
 import com.dev.reachforhelp.databinding.FragmentNotificationsBinding
+import com.dev.reachforhelp.model.NotificationData
+import com.dev.reachforhelp.ui.adapter.NotificationAdapter
 
 class NotificationsFragment : Fragment() {
 
     private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private  var _binding: FragmentNotificationsBinding? = null
+    private lateinit var recyclerviewNotify: RecyclerView
+    private lateinit var notificationAdapter: NotificationAdapter
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val notification = mutableListOf<NotificationData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,15 +37,55 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textNotifications
-//        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
-        return root
+
+        notificationsViewModel.notificationList.observe(viewLifecycleOwner, Observer {
+           notification.addAll(notification)
+        })
+
+        var notificationList = listOf(
+            NotificationData(R.drawable.girl,
+                "Your report has been submitted successfully", "20hrs ago"),
+            NotificationData(R.drawable.girl,
+            "Your report has been submitted successfully", "18hrs ago"),
+            NotificationData(R.drawable.notity_img,
+                "Your report has been treated", "18hrs ago"),
+            NotificationData(R.drawable.girl,
+                "Your report has been submitted successfully", "16hrs ago"),
+            NotificationData(R.drawable.re,
+                "Your report has been submitted successfully", "13hrs ago"),
+            NotificationData(R.drawable.notity_img,
+                "Your report has been treated", "11hrs ago"),
+            NotificationData(R.drawable.girl,
+                "Your report is been looked into", "10hrs ago"),
+            NotificationData(R.drawable.notity_img,
+                "Your report has been submitted successfully", "10hrs ago"),
+            NotificationData(R.drawable.girl,
+                "Your report is been looked into", "9hrs ago"),
+            NotificationData(R.drawable.re,
+                "Your report has been treated", "8hrs ago"),
+            NotificationData(R.drawable.notity_img,
+                "Your report has been treated", "6hrs ago"),
+            NotificationData(R.drawable.re,
+                "Your report has been submitted successfully", "4hrs ago"),
+            NotificationData(R.drawable.girl,
+                "Your report has been treated", "9hrs ago"),
+        )
+
+        binding.recyclerViewNotification.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.recyclerViewNotification.adapter =
+            NotificationAdapter(notificationList, requireContext())
+
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentNotificationsBinding.bind(view)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
--- app/src/main/java/com/dev/reachforhelp/ui/eyeWitness/EyeWitnessFragment.kt
+++ app/src/main/java/com/dev/reachforhelp/ui/eyeWitness/EyeWitnessFragment.kt
@@ -1,26 +1,39 @@
 package com.dev.reachforhelp.ui.eyeWitness
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.dev.reachforhelp.R
+import com.dev.reachforhelp.databinding.EyeWitnessFragmentBinding
 
 class EyeWitnessFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = EyeWitnessFragment()
-    }
+//    companion object {
+//        fun newInstance() = EyeWitnessFragment()
+//    }
 
     private lateinit var viewModel: EyeWitnessViewModel
+    private lateinit var binding: EyeWitnessFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.eye_witness_fragment, container, false)
+        binding = EyeWitnessFragmentBinding.inflate(layoutInflater)
+
+        binding.eyewitinessVideoclip.setOnClickListener {
+            findNavController().navigate(R.id.action_eyeWitnessFragment_to_videoClipFragment)
+        }
+        binding.voicenoteEyewitness.setOnClickListener {
+            findNavController().navigate(R.id.action_eyeWitnessFragment_to_voiceNoteFragment)
+        }
+        return binding.root
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
