7b22de94763590d965dce2420176a7cb56ae7f7e
Peculiaruc <peculiarumeh02@gmail.com>
2022-03-03 23:16:53 +0100

    Implemented Help Screen, Dashboard Screen, Notification Screen

---
+++ app/src/main/java/com/dev/reachforhelp/ui/dashboard/ui/help/HelpViewModel.kt
@@ -0,0 +1,13 @@
+package com.dev.reachforhelp.ui.dashboard.ui.help
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HelpViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
